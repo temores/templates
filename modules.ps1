2f77e77f655ce9cca276c79f72a1db04544a44e9
adrian corona <acorona@microsoft.com>
2019-05-06 18:21:56 -0500

    Update modules.ps1

--- modules.ps1
+++ modules.ps1
@@ -1,3 +1,5 @@
 Install-PackageProvider -Name NuGet -force
 Install-Module -Name NetworkingDsc -force
 Install-Module -Name xSystemSecurity -force
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
